a9516da9731b83a4c5763d34ec2af7e9d93635b0
erdeszt <erdeszt@gmail.com>
2020-06-11 00:13:13 +0200

    Working ide and demo program

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,26 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Ivory.Language
--- import Iv
+import Ivory.Compile.C.CmdlineFrontend
 
--- puts :: Def ('[IString] :-> Sint32)
--- puts  = importProc "puts" "stdio.h"
---
--- mainProc :: Def ('[] :-> ())
--- mainProc  = proc "main" $ body $ do
---   call_ puts "hello, world\n"
---   retVoid
+puts :: Def ('[IString] :-> Sint32)
+puts  = importProc "puts" "stdio.h"
 
--- mainModule :: Module
+mainProc :: Def ('[] :-> ())
+mainProc  = proc "main" $ body $ do
+  call_ puts "hello, world\n"
+  retVoid
+
+mainModule :: Module
+mainModule = package "test" $ do
+  incl puts
+  incl mainProc
 
 main :: IO ()
 main = do
-  -- runCompiler[mainProc]
-  print "YO"
+  runCompiler [mainModule] [] initialOpts { outDir = Nothing }
   return ()
