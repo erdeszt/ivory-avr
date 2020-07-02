5a72e5b8da533573526db80c12caf7b7231e3642
erdeszt <erdeszt@gmail.com>
2020-07-02 11:13:50 +0200

    Implement delay

--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,14 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import Ivory.Compile.C.CmdlineFrontend
 import Ivory.HW
 import Ivory.Language
+import GHC.TypeNats
 
 -- TODO:
 -- constexpr u8 SFR_OFFSET = __AVR_ARCH__ >= 100 ? 0x00 : 0x20;
@@ -63,26 +65,43 @@ reg_DDRB = mkBitDataReg 0x04
 reg_PORTB :: BitDataReg PORTB
 reg_PORTB = mkBitDataReg 0x05
 
-delayMS :: Def ('[Uint16] :-> ())
-delayMS = proc "delay" $ \interval -> body $ do
+delayInit :: Def ('[] :-> ())
+delayInit = proc "delay_init" $ body $ do
+    modifyReg reg_TCCR1B $ do
+      setBit cs10
     retVoid
+
+delayMS :: Def ('[Ix 10000] :-> ())
+delayMS = proc "delay" $ \interval -> body $ do
+    writeReg reg_TCNT1 0
+    interval `times` \_ -> do
+      maxLoopCount `times` \_ -> do
+        counterValue <- readReg reg_TCNT1
+        ifte_ (counterValue >? 16000) breakOut (return ())
+    retVoid
+  where
+    maxLoopCount :: Ix 100000
+    maxLoopCount = 100000
 
 mainProc :: Def ('[] :-> ())
 mainProc = proc "main" $ body $ do
+    call_ delayInit
     modifyReg reg_DDRB $ do
         setBit ddrb1
     forever $ do
         modifyReg reg_PORTB $ do
             setBit portb1
-        -- delay
+        call_ delayMS 1000
         modifyReg reg_PORTB $ do
             clearBit portb1
-        -- delay
+        call_ delayMS 1000
     retVoid
 
 mainModule :: Module
 mainModule = package "firmware" $ do
     hw_moduledef
+    incl delayInit
+    incl delayMS
     incl mainProc
 
 main :: IO ()
