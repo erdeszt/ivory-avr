707d3a3e752218b206bde6d816d49926d628f84b
erdeszt <erdeszt@gmail.com>
2020-07-18 12:48:36 +0200

    Add simulation build

---
+++ src/sim/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <libgen.h>
+#include <pthread.h>
+
+#include <sim_avr.h>
+#include <avr_twi.h>
+#include <sim_elf.h>
+#include <sim_gdb.h>
+#include <sim_vcd_file.h>
+
+int main(void)
+{
+    printf("YO\n");
+
+    return 0;
+}
